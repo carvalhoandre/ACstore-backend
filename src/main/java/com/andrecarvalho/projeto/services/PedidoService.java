99985d2abc1def52d90bbbe2f23ceea75672ca49
André Carvalho <andre_carvalho0@live.com>
2020-09-17 18:47:44 -0300

    Implementando toString de pedido

--- src/main/java/com/andrecarvalho/projeto/services/PedidoService.java
+++ src/main/java/com/andrecarvalho/projeto/services/PedidoService.java
@@ -2,9 +2,11 @@ package com.andrecarvalho.projeto.services;
 
 import java.util.Date;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.andrecarvalho.projeto.domain.ItemPedido;
 import com.andrecarvalho.projeto.domain.PagamentoComBoleto;
 import com.andrecarvalho.projeto.domain.Pedido;
@@ -32,6 +34,9 @@ public class PedidoService {
 	@Autowired
 	private ProdutoService produtoService;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 
 	public Pedido find(Integer id) {
 		 Optional<Pedido> obj = repo.findById(id);
@@ -43,6 +48,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if(obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -53,10 +59,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
