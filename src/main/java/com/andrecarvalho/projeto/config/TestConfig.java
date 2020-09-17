18a5d8b4293c267c8be7f9d8c433ea63367e136a
André Carvalho <andre_carvalho0@live.com>
2020-09-17 19:23:37 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/andrecarvalho/projeto/config/TestConfig.java
+++ src/main/java/com/andrecarvalho/projeto/config/TestConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import com.andrecarvalho.projeto.services.DBService;
+import com.andrecarvalho.projeto.services.EmailService;
+import com.andrecarvalho.projeto.services.MockEmailService;
 
 @Configuration
 @Profile("test")
@@ -18,5 +20,10 @@ public class TestConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
