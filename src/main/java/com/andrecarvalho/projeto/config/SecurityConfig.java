1f0526aacc04a9e5fa5f94d2e1bf6bace5b0fc96
André Carvalho <andre_carvalho0@live.com>
2020-09-23 19:01:00 -0300

    Implementando autorizacao

--- src/main/java/com/andrecarvalho/projeto/config/SecurityConfig.java
+++ src/main/java/com/andrecarvalho/projeto/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.andrecarvalho.projeto.security.JWTAuthenticationFilter;
+import com.andrecarvalho.projeto.security.JWTAuthorizationFilter;
 import com.andrecarvalho.projeto.security.JWTUtil;
 
 @Configuration
@@ -67,6 +68,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
