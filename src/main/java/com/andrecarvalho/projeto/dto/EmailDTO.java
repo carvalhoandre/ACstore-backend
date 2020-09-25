adfcf52acc1497c316aee8ff3d81ee0caa458b8a
André Carvalho <andre_carvalho0@live.com>
2020-09-25 19:11:33 -0300

    Esqueci a senha

---
+++ src/main/java/com/andrecarvalho/projeto/dto/EmailDTO.java
@@ -0,0 +1,28 @@
+package com.andrecarvalho.projeto.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class EmailDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String email;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="E-mail inválido")
+	public EmailDTO() {}
+
+	public EmailDTO(String email) {
+		super();
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
