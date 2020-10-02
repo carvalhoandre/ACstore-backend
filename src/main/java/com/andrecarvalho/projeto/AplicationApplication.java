a5f5fa79e5d1f6421c5c81ccdc075907d6b84aaa
André Carvalho <andre_carvalho0@live.com>
2020-10-02 20:03:15 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/andrecarvalho/projeto/AplicationApplication.java
+++ src/main/java/com/andrecarvalho/projeto/AplicationApplication.java
@@ -1,16 +1,24 @@
 package com.andrecarvalho.projeto;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.andrecarvalho.projeto.services.S3Service;
+
 @SpringBootApplication
 public class AplicationApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AplicationApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {}
+	public void run(String... args) throws Exception {
+		s3Service.uploadFile("D:\\andre\\Pictures\\Saved Pictures\\Duff.jpg");
+	}
 }
