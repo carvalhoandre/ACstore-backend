ceadb1f93566012a26cbe8ad10e188eaa6654152
André Carvalho <andre_carvalho0@live.com>
2020-10-12 16:39:30 -0300

    Usando padrão de nomes para imagens

--- src/main/java/com/andrecarvalho/projeto/services/ClienteService.java
+++ src/main/java/com/andrecarvalho/projeto/services/ClienteService.java
@@ -1,10 +1,12 @@
 package com.andrecarvalho.projeto.services;
 
+import java.awt.image.BufferedImage;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -42,6 +44,12 @@ public class ClienteService {
 	
 	@Autowired
 	private S3Service s3Service;
+	
+	@Autowired
+	private ImageService imageService;
+	
+	@Value("${img.prefix.client.profile}")
+	private String prefix;
 
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -115,10 +123,8 @@ public class ClienteService {
 		if(user == null) {
 			throw new AuthorizationException("Acesso negado");
 		}
-		URI uri = s3Service.uploadFile(multipartFile);
-		Cliente cli = repo.findByEmail(user.getUsername());
-		cli.setImageUrl(uri.toString());
-		repo.save(cli);
-		return uri;
+		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		String fileName = prefix + user.getIde() + ".jpg";
+		return s3Service.uploadFile(imageService.getInputStrean(jpgImage, "jpg"), fileName, "image");
 	}
 }
